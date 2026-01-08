416f92afc44790bee28a5fae45f9fe0d07205ec3
alexwoodhx <woodsy366@gmail.com>
2026-01-08 15:58:03 +0000

    Add further auth logic with routes and config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import healthRoutes from './routes/health.routes.js';
+import authRoutes from './modules/auth/auth.routes.js'
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/health', healthRoutes);
+app.use("/auth", authRoutes);
 
 export default app;
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,6 +1,6 @@
-import User from "./user.model";
-import jwt from "jsonwebtoken"
-import { env } from "../../config/env";
+import User from "./user.model.js";
+import jwt from "jsonwebtoken";
+import { env } from "../../config/env.js";
 
 //check if user already exists, if not, create new user
 export const registerUser = async ({email, password}) => {
@@ -18,7 +18,7 @@ export const loginUser = async ({email, password}) => {
     const user = await User.findOne({ email });
     if (!user) throw new Error("Invalid email");
 
-    const isMatch = user.comparePassword(password);
+    const isMatch = await user.comparePassword(password);
     if (!isMatch) throw new Error("Invalid password");
 
     const token = jwt.sign( {userId: user._id}, env.jwtSecret, {
