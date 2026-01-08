a5f42d1ecfb6473fed255c7f471668a993180b11
alexwoodhx <woodsy366@gmail.com>
2026-01-08 14:16:22 +0000

    Create User model with password hashing and comparison

---
+++ src/modules/auth/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Hash password pre-save
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+//Password comparison method
+userSchema.methods.comparePassword =  async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password)
+};
+    
+const User = mongoose.model("User", userSchema);
+export default User;
