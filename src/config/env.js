ccc929f04a7829ab10955a64eb5068cb52156cc3
alexwoodhx <woodsy366@gmail.com>
2026-01-08 13:52:22 +0000

    add mongoDB connection and configuration

--- src/config/env.js
+++ src/config/env.js
@@ -4,5 +4,11 @@ dotenv.config();
 
 export const env = {
    port: process.env.PORT || 3000,
-    nodeEnv: process.env.NODE_ENV || 'development',
+   nodeEnv: process.env.NODE_ENV || 'development',
+   mongoUri: process.env.MONGODB_URI,
 };
+
+// Check if all required environment variables are set
+if (!env.mongoUri) {
+    throw new Error('MONGO_URI is not set, please check your .env file');
+}
