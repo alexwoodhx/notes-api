58f2e2ce63a76f79bd6a7494c543f4283041b373
alexwoodhx <woodsy366@gmail.com>
2026-01-09 10:37:45 +0000

    Finalise and wire up validation

--- src/modules/notes/notes.routes.js
+++ src/modules/notes/notes.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authMiddleware } from "../../middleware/auth.middleware.js";
+import { validate } from "../../middleware/validate.middleware.js";
 import {
     create,
     list,
@@ -7,16 +8,21 @@ import {
     update,
     remove
 } from "./notes.controller.js";
+import {
+    createNoteValidation,
+    updateNoteValidation,
+    noteIdValidation
+} from "./notes.validation.js";
 
 const router = Router();
 
 //implement middleware for protection
 router.use(authMiddleware);
 
-router.post("/", create);
+router.post("/", createNoteValidation, validate, create);
 router.get("/", list);
-router.get("/:id", getOne);
-router.put("/:id", update);
-router.delete("/:id", remove);
+router.get("/:id", noteIdValidation, validate, getOne);
+router.put("/:id", noteIdValidation, updateNoteValidation, validate, update);
+router.delete("/:id", noteIdValidation, validate, remove);
 
 export default router;
