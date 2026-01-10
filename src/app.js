6304736577666e60ae484838ca51c795f26503f3
alexwoodhx <woodsy366@gmail.com>
2026-01-10 22:04:13 +0000

    set trust proxy for render deployment

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ import { globalLimiter, authLimiter } from './middleware/rateLimit.middleware.js
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(helmet());
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json({ limit: "10kb" }));
