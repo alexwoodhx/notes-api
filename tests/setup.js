c5e6fd1451476f2baaaf7c4700692e32104aaec0
alexwoodhx <woodsy366@gmail.com>
2026-01-10 17:31:10 +0000

    Install testing dependencies and initial test setup

---
+++ tests/setup.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import app from "../src/app.js";
+
+let mongoServer;
+
+export const connectDB = async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri, { dbName: "test" });    
+};
+
+export const closeDB = async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+};
+
+export const clearDB = async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+        await collections[key].deleteMany({});
+    }
+};
+
+export { app };
